897a4c70ad8cce3f9626547386d8b1e2a6f011ac
Gianluca Fernandes <gianfernandes1@gmail.com>
2025-01-15 18:09:55 -0300

    Adicionando calculo area do circulo e informacoes de data

--- exercicios.py
+++ exercicios.py
@@ -1,3 +1,7 @@
+import math
+from datetime import datetime
+
+#Programa para calcular a divisão inteira entre números inteiros
 primeiro_numero = int(input("Digite um número inteiro: "))
 
 segundo_numero = int(input("Digite outro número inteiro: "))
@@ -10,3 +14,19 @@ print(f"A divisão inteira de {primeiro_numero} por {segundo_numero} é {divisao
 
 if (resto_divisao != 0):
   print(f"E o resto da divisão é {resto_divisao}")
+
+#Programa para calcular a área de um círculo, recebendo o raio como entrada
+raio = float(input("Digite o raio do círculo: "))
+
+area = math.pi * raio**2
+
+print(f"A área do círculo é {area:.2f}")
+
+#Programa para informar o dia, o ano e o mês
+data_str = input("Digite uma data (dd/mm/aaaa): ")
+
+data_obj = datetime.strptime(data_str, '%d/%m/%Y')
+
+print(f"O dia é {data_obj.day}")
+print(f"O mês é {data_obj.month}")
+print(f"O ano é {data_obj.year}")
